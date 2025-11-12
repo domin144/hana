3e556443a58c6726562bd39b353dab612b3668a0
Dominik Wójt <domin144@o2.pl>
2025-11-12 20:32:14 +0100

    avoid copies, when comparing maps. Alternative: wrapped at_map to force cast in partial
    There were two points, where copies were made:
    
    1. When binding a map for use in `partial`.
    
    This could be avoided, by using `std::ref`, but then either `partial`
    would have to handle `std::ref` in special way, by unwrapping the
    reference at the call point or the wrapped function would have to
    specify the accepted argument type explicitly to force cast.
    
    Instead, partial was replaced with a labda capturing the `maps` by
    reference.
    
    2. When returning the value from `find` wrapped into optional.
    
    This is avoided by using `at_key` instead of `find`.

--- include/boost/hana/map.hpp
+++ include/boost/hana/map.hpp
@@ -13,6 +13,7 @@ Distributed under the Boost Software License, Version 1.0.
 #include <boost/hana/fwd/map.hpp>
 
 #include <boost/hana/all_of.hpp>
+#include <boost/hana/at_key.hpp>
 #include <boost/hana/basic_tuple.hpp>
 #include <boost/hana/bool.hpp>
 #include <boost/hana/concept/comparable.hpp>
@@ -39,7 +40,6 @@ Distributed under the Boost Software License, Version 1.0.
 #include <boost/hana/functional/on.hpp>
 #include <boost/hana/functional/partial.hpp>
 #include <boost/hana/fwd/any_of.hpp>
-#include <boost/hana/fwd/at_key.hpp>
 #include <boost/hana/fwd/difference.hpp>
 #include <boost/hana/fwd/erase_key.hpp>
 #include <boost/hana/fwd/intersection.hpp>
@@ -53,11 +53,13 @@ Distributed under the Boost Software License, Version 1.0.
 #include <boost/hana/optional.hpp>
 #include <boost/hana/remove_if.hpp>
 #include <boost/hana/second.hpp>
+#include <boost/hana/set.hpp>
 #include <boost/hana/unpack.hpp>
 #include <boost/hana/value.hpp>
 
 
 #include <cstddef>
+#include <functional>
 #include <type_traits>
 #include <utility>
 
@@ -368,20 +370,33 @@ namespace boost { namespace hana {
             return hana::false_c;
         }
 
+        // This is needed, so that the function argument is explicit and
+        // cast from std::ref<const M> to const M& is forced.
+        // Without this hana::at_key would receive std::ref<const M> as argument
+        // and panic.
+        template<typename M> 
+        struct at_key_helper
+        {
+            template<typename K>
+            constexpr decltype(auto) operator()(const M& m, const K& k) const {
+                return hana::at_key(m, k);
+            }
+        };
+
         template <typename M1, typename M2>
         static constexpr auto equal_helper(M1 const& m1, M2 const& m2, hana::true_) {
             return hana::all_of(hana::keys(m1), hana::demux(equal)(
-                hana::partial(hana::find, m1),
-                hana::partial(hana::find, m2)
+                hana::partial(equal_impl::at_key_helper<M1>(), std::ref(m1)),
+                hana::partial(equal_impl::at_key_helper<M2>(), std::ref(m2))
             ));
         }
 
         template <typename M1, typename M2>
         static constexpr auto apply(M1 const& m1, M2 const& m2) {
-            return equal_impl::equal_helper(m1, m2, hana::bool_c<
-                decltype(hana::length(m1.storage))::value ==
-                decltype(hana::length(m2.storage))::value
-            >);
+            return equal_impl::equal_helper(m1, m2, 
+                    hana::to_set(hana::keys(m1)) ==
+                    hana::to_set(hana::keys(m2))
+            );
         }
     };
 
